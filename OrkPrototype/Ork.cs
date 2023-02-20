541ff82716d76e8b98db3dd428b9a2f39fa97f29
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-02-20 14:15:27 +0100

    Prototype

---
+++ OrkPrototype/Ork.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace OrkPrototype
+{
+    internal class Ork
+    {     
+        public int Age { get; set; }
+        public int Strenght { get; set; }
+        public int Speed { get; set; }
+
+        public Ork copyOrk()
+        {
+            string orkCopy = JsonConvert.SerializeObject(this);
+            Ork copiedOrk = JsonConvert.DeserializeObject<Ork>(orkCopy);
+            copiedOrk.Strenght = new Random().Next(300);
+            return copiedOrk;
+        }
+    }
+}
---
+++ OrkPrototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using OrkPrototype;
+
+List<Ork> battleOrks = new();
+
+var ork = new Ork();
+ork.Age = 100;
+ork.Strenght = 200;
+ork.Speed = 50;
+
+battleOrks.Add(ork);
+
+Ork cloneOrk = ork.copyOrk();
+Ork cloneOrk2 = ork.copyOrk();
+battleOrks.Add(cloneOrk);
+battleOrks.Add(cloneOrk2);
+battleOrks.Add(ork.copyOrk());
+
+foreach (var item in battleOrks)
+{
+    Console.WriteLine($"Cześć moja siła to {item.Strenght}, " +
+        $"mój wiek to {item.Age}, moja szybkość to {item.Speed} {item.GetType()}");
+}
+
