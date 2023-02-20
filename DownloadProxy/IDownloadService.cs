a55ac01179a52cccc165a81a563b28773d371455
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-02-20 16:00:22 +0100

    Proxy

---
+++ DownloadProxy/IDownloadService.cs
@@ -0,0 +1,7 @@
+﻿namespace DownloadProxy
+{
+    internal interface IDownLoadService
+    {
+        public void DownloadFilm(string title, string password);
+    }
+}
---
+++ DownloadProxy/Program.cs
@@ -0,0 +1,8 @@
+﻿using DownloadProxy;
+var downloadService = new DownloadService();
+downloadService.DownloadFilm("Pocahontas", "Test1234");
+downloadService.DownloadFilm("Avatar 2", "TestTest");
+
+var downloadServiceProxy = new DownloadServiceProxy();
+downloadServiceProxy.DownloadFilm("Pocahontas", "Test1234");
+downloadServiceProxy.DownloadFilm("Avatar 2", "TestTest");
